018119c7c7ccdab16decc781f8f3b20212081a32
Igor Gonçalves <igordossantos38@gmail.com>
2022-03-05 11:08:59 -0300

    Atualização geral do código

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,31 +4,34 @@ function carregar() {
     const img = document.querySelector('#image');
     const greetings = document.querySelector('#greetings');
     const data = new Date();
-    let hora = data.getHours() + 3;
+    let hora = data.getHours();
     let min = data.getMinutes();
     min < 10 ? min = '0' + min : 0;
     hora < 10 ? hora = '0' + hora : 0;
     msg.innerHTML = `${hora}:${min}`;
-    if (hora >= 0 && hora < 12) {
-        img.src = '../img/manha.jpg';
-        document.body.style.background =
-            `linear-gradient(to right, #d7e060d0, #e9d736d0),
-        url()`;
-        greetings.innerHTML = 'Bom Dia!';
+    function mudancaDeHorario() {
+        if (hora >= 0 && hora < 12) {
+            img.src = './assets/img/manha.jpg';
+            document.body.style.backgroundImage =
+                `linear-gradient(to right, #d7e060d0, #e9d736d0),
+                url(./assets/img/bgmorning.jpg)`;
+            greetings.innerHTML = 'Bom Dia!';
+        }
+        else if (hora >= 12 && hora < 18) {
+            img.src = './assets/img/tarde.jpg';
+            document.body.style.background =
+                `linear-gradient(to right, #ffc500dc, #c21500dc),
+                    url(./assets/img/bgafternoon.jpg)`;
+            greetings.innerHTML = 'Boa Tarde!';
+        }
+        else {
+            img.src = '../img/noite.jpg';
+            document.body.style.background =
+                `linear-gradient(to right, #072530d0, #111129d0),
+                    url(./assets/img/bgnight.jpg)`;
+            greetings.innerHTML = 'Boa Noite!';
+        }
     }
-    else if (hora >= 12 && hora < 18) {
-        img.src = '../img/tarde.jpg';
-        document.body.style.background =
-            `linear-gradient(to right, #ffc500dc, #c21500dc),
-            url(../img/bgafternoon.jpg)`;
-        greetings.innerHTML = 'Boa Tarde!';
-    }
-    else {
-        img.src = '../img/noite.jpg';
-        document.body.style.background =
-            `linear-gradient(to right, #072530d0, #111129d0),
-            url(../img/bgnight.jpg)`;
-        greetings.innerHTML = 'Boa Noite!';
-    }
+    mudancaDeHorario();
 }
 //# sourceMappingURL=index.js.map
--- src/assets/js/index.ts
+++ src/assets/js/index.ts
@@ -4,39 +4,44 @@ function carregar(): any {
     const greetings: any = document.querySelector('#greetings');
     const data: any = new Date();
 
-    let hora: any = data.getHours() + 3;
-    let min: any = data.getMinutes();
+    let hora: number | string = data.getHours();
+    let min: number | string = data.getMinutes();
     
     min < 10 ? min = '0' + min : 0
     hora < 10 ? hora = '0' + hora : 0
 
     msg.innerHTML = `${hora}:${min}`;
 
-    if (hora >= 0 && hora < 12) {
-        img.src = '../img/manha.jpg';
-
-        document.body.style.background = 
-        `linear-gradient(to right, #d7e060d0, #e9d736d0),
-        url()`;
-
-        greetings.innerHTML = 'Bom Dia!';
+    function mudancaDeHorario (): void  {
         
-    } else if (hora >= 12 && hora < 18) {
-            img.src = '../img/tarde.jpg';
-            
-            document.body.style.background = 
-            `linear-gradient(to right, #ffc500dc, #c21500dc),
-            url(../img/bgafternoon.jpg)`;
-
-            greetings.innerHTML = 'Boa Tarde!';
-
-        } else {
-            img.src = '../img/noite.jpg';
-
-            document.body.style.background = 
-            `linear-gradient(to right, #072530d0, #111129d0),
-            url(../img/bgnight.jpg)`;
-
-            greetings.innerHTML = 'Boa Noite!';
-        }
+            if (hora >= 0 && hora < 12) {
+                img.src = './assets/img/manha.jpg';
+                
+        
+                document.body.style.backgroundImage = 
+                `linear-gradient(to right, #d7e060d0, #e9d736d0),
+                url(./assets/img/bgmorning.jpg)`;
+        
+                greetings.innerHTML = 'Bom Dia!';
+                
+            } else if (hora >= 12 && hora < 18) {
+                    img.src = './assets/img/tarde.jpg';
+                    
+                    document.body.style.background = 
+                    `linear-gradient(to right, #ffc500dc, #c21500dc),
+                    url(./assets/img/bgafternoon.jpg)`;
+        
+                    greetings.innerHTML = 'Boa Tarde!';
+        
+                } else {
+                    img.src = '../img/noite.jpg';
+        
+                    document.body.style.background = 
+                    `linear-gradient(to right, #072530d0, #111129d0),
+                    url(./assets/img/bgnight.jpg)`;
+        
+                    greetings.innerHTML = 'Boa Noite!';
+                }
+            }
+            mudancaDeHorario()
 }
