1f85224b626bd8824f9e0df80f2fd2bfbb7fe49a
Igor Gonçalves <igordossantos38@gmail.com>
2022-03-05 14:12:18 -0300

    Finalizando mudanças

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,20 +4,19 @@ function carregar() {
     const img = document.querySelector('#image');
     const greetings = document.querySelector('#greetings');
     const data = new Date();
-    let hora = data.getHours();
-    let min = data.getMinutes();
-    min < 10 ? min = '0' + min : 0;
-    hora < 10 ? hora = '0' + hora : 0;
-    msg.innerHTML = `${hora}:${min}`;
+    let hora = data.toLocaleTimeString('pt-BR', { timeZone: 'America/Sao_Paulo' });
+    const newHora = parseInt(hora);
+    const sec = setInterval(carregar, 1000);
+    msg.innerHTML = hora;
     function mudancaDeHorario() {
-        if (hora >= 0 && hora < 12) {
+        if (newHora == 0 && newHora < 12) {
             img.src = './assets/img/manha.jpg';
-            document.body.style.backgroundImage =
+            document.body.style.background =
                 `linear-gradient(to right, #d7e060d0, #e9d736d0),
                 url(./assets/img/bgmorning.jpg)`;
             greetings.innerHTML = 'Bom Dia!';
         }
-        else if (hora >= 12 && hora < 18) {
+        else if (newHora >= 12 && newHora < 18) {
             img.src = './assets/img/tarde.jpg';
             document.body.style.background =
                 `linear-gradient(to right, #ffc500dc, #c21500dc),
@@ -25,7 +24,7 @@ function carregar() {
             greetings.innerHTML = 'Boa Tarde!';
         }
         else {
-            img.src = '../img/noite.jpg';
+            img.src = './assets/img/noite.jpg';
             document.body.style.background =
                 `linear-gradient(to right, #072530d0, #111129d0),
                     url(./assets/img/bgnight.jpg)`;
--- src/assets/js/index.ts
+++ src/assets/js/index.ts
@@ -1,47 +1,45 @@
 function carregar(): any {
-    const msg:any  = document.querySelector('#msg');
-    const img: any = document.querySelector('#image');
-    const greetings: any = document.querySelector('#greetings');
-    const data: any = new Date();
+    const msg:any  = document.querySelector('#msg')
+    const img: any = document.querySelector('#image')
+    const greetings: any = document.querySelector('#greetings')
 
-    let hora: number | string = data.getHours();
-    let min: number | string = data.getMinutes();
+    const data: any = new Date()
+    let hora: any = data.toLocaleTimeString('pt-BR', {timeZone: 'America/Sao_Paulo'})
     
-    min < 10 ? min = '0' + min : 0
-    hora < 10 ? hora = '0' + hora : 0
+    const newHora: number = parseInt(hora)
 
-    msg.innerHTML = `${hora}:${min}`;
-
+    const sec = setInterval(carregar ,1000)
+    msg.innerHTML = hora
     function mudancaDeHorario (): void  {
         
-            if (hora >= 0 && hora < 12) {
+            if (newHora == 0 && newHora < 12) {
                 img.src = './assets/img/manha.jpg';
                 
         
-                document.body.style.backgroundImage = 
+                document.body.style.background = 
                 `linear-gradient(to right, #d7e060d0, #e9d736d0),
-                url(./assets/img/bgmorning.jpg)`;
+                url(./assets/img/bgmorning.jpg)`
         
-                greetings.innerHTML = 'Bom Dia!';
+                greetings.innerHTML = 'Bom Dia!'
                 
-            } else if (hora >= 12 && hora < 18) {
-                    img.src = './assets/img/tarde.jpg';
+            } else if (newHora >= 12 && newHora < 18) {
+                    img.src = './assets/img/tarde.jpg'
                     
                     document.body.style.background = 
                     `linear-gradient(to right, #ffc500dc, #c21500dc),
-                    url(./assets/img/bgafternoon.jpg)`;
+                    url(./assets/img/bgafternoon.jpg)`
         
-                    greetings.innerHTML = 'Boa Tarde!';
+                    greetings.innerHTML = 'Boa Tarde!'
         
                 } else {
-                    img.src = '../img/noite.jpg';
+                    img.src = './assets/img/noite.jpg'
         
                     document.body.style.background = 
                     `linear-gradient(to right, #072530d0, #111129d0),
-                    url(./assets/img/bgnight.jpg)`;
+                    url(./assets/img/bgnight.jpg)`
         
-                    greetings.innerHTML = 'Boa Noite!';
+                    greetings.innerHTML = 'Boa Noite!'
                 }
             }
-            mudancaDeHorario()
+    mudancaDeHorario()
 }
